81d9835fb92c4b4356a2cd272da1bddfef7024d8
Camperkunz <anyanikiforova04@gmail.com>
2026-03-15 14:45:09 -0400

    feat: google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import ProjectPage from "./pages/ProjectPage";
 import AllProjectsPage from "./pages/AllProjectsPage";
@@ -11,12 +13,30 @@ import ScrollToTop from "./components/portfolio/ScrollToTop";
 
 const queryClient = new QueryClient();
 
+// 1. Google Analytics
+ReactGA.initialize("G-5VDQD7BG9X");
+
+// 2. Tracking component
+const AnalyticsTracker = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search
+    });
+  }, [location]);
+
+  return null;
+};
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <AnalyticsTracker />
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
